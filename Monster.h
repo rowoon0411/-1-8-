3e825ab7e60732a5c6fb1842ffabb28cdd7b8215
rowoon0411 <ascrewball1231@gmail.com>
2026-05-20 16:13:36 +0900

    Create Monster.h

---
+++ Monster.h
@@ -0,0 +1,145 @@
+#pragma once
+
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+void printStatus(string name, int stat[]);
+void upgradeMenu(string name, int stat[]);
+
+// =====================
+// 아이템 구조체
+// =====================
+struct Item
+{
+    string name;
+    int price;
+
+    void PrintInfo() const;
+};
+
+// =====================
+// 포션 레시피
+// =====================
+struct PotionRecipe
+{
+    string potionName;
+    vector<string> ingredients;
+
+    void ShowRecipe() const;
+};
+
+// =====================
+// 포션 제작소
+// =====================
+class AlchemyWorkshop
+{
+private:
+    vector<PotionRecipe> recipes;
+
+public:
+    AlchemyWorkshop();
+
+    void ShowAllRecipes();
+    void SearchByName(string name);
+    void SearchByIngredient(string ingredient);
+    void OpenWorkshop();
+};
+
+// =====================
+// Player 클래스
+// =====================
+class Player
+{
+protected:
+    string name;
+    string job;
+
+    int level;
+    int hp;
+    int mp;
+    int power;
+    int defence;
+
+public:
+    Player(string name, string job, int hp, int mp, int power, int defence);
+
+    string getName();
+    string getJob();
+
+    int getHP();
+    int getPower();
+    int getDefence();
+
+    void setHP(int newHP);
+
+    void printPlayerStatus();
+
+    virtual void attack() = 0;
+
+    virtual ~Player() {}
+};
+
+class Warrior : public Player
+{
+public:
+    Warrior(string name, int hp, int mp, int power, int defence);
+    void attack() override;
+};
+
+class Magician : public Player
+{
+public:
+    Magician(string name, int hp, int mp, int power, int defence);
+    void attack() override;
+};
+
+class Thief : public Player
+{
+public:
+    Thief(string name, int hp, int mp, int power, int defence);
+    void attack() override;
+};
+
+class Archer : public Player
+{
+public:
+    Archer(string name, int hp, int mp, int power, int defence);
+    void attack() override;
+};
+
+// =====================
+// Monster 클래스
+// =====================
+class Monster
+{
+private:
+    string name;
+    int hp;
+    int power;
+    int defence;
+
+    string dropItemName;
+    int dropItemPrice;
+
+public:
+    Monster(string name, int hp, int power, int defence,
+        string dropItemName, int dropItemPrice);
+
+    string getName();
+    int getHP();
+    int getPower();
+    int getDefence();
+
+    string getDropItemName();
+    int getDropItemPrice();
+
+    void setHP(int newHP);
+
+    void attack(Player* player);
+};
+
+void battle(Player* player, Monster& monster, vector<Item>& inventory);
+void mainMenu(Player* player, vector<Item>& inventory);
